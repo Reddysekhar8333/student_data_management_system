26c83a4f59dbab3123a5fe47dc8536df6d0055a7
Reddysekhar8333 <srsekhar8333@gmail.com>
2025-11-17 16:03:05 +0530

    1st

--- modules/delete_student.py
+++
@@ -1,9 +0,0 @@
-from database.db_config import get_connection
-
-def delete_student(student_id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM students WHERE id = %s", (student_id,))
-    conn.commit()
-    conn.close()
-    print(" Student deleted successfully!")
--- modules/view_students.py
+++
@@ -1,9 +0,0 @@
-from database.db_config import get_connection
-
-def view_students():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM students")
-    for row in cursor.fetchall():
-        print(row)
-    conn.close()
